ac8a3c1737ba15b07f8e24567e425d7963df9507
Harleen Singh <harleensingh0001@gmail.com>
2021-01-26 05:11:07 +0530

    Create AppSearchController.swift

---
+++ AppStore Clone/Controller/AppSearchController.swift
@@ -0,0 +1,30 @@
+//
+//  AppSearchController.swift
+//  AppStore Clone
+//
+//  Created by Harleen Singh on 26/01/21.
+//
+
+import UIKit
+
+
+
+class AppSearchController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .white
+    }
+    
+    
+    // use init or provide UICollectionViewLayout as constructor
+//    init() {
+//        super.init(collectionViewLayout: UICollectionViewLayout())
+//    }
+//    
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+    
+   
+}
