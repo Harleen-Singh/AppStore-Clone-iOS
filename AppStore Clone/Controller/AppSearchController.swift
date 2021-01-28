2099badba236ee59cd17c038eeee807c3d564caf
Harleen Singh <harleensingh0001@gmail.com>
2021-01-29 03:20:59 +0530

    add services and api call to search collectionview
    For image load SDWebImage used

--- AppStore Clone/Controller/AppSearchController.swift
+++ AppStore Clone/Controller/AppSearchController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import SDWebImage
 
 
 class AppSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
@@ -18,27 +18,46 @@ class AppSearchController: UICollectionViewController, UICollectionViewDelegateF
         collectionView.backgroundColor = .white
         
         collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: cellId)
+        
+        fetchITunesApps()
     }
     
-    // use init or provide UICollectionViewLayout as constructor
-//    init() {
-//        super.init(collectionViewLayout: UICollectionViewLayout())
-//    }
-//
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
+    
+    fileprivate var appResults = [Result]()
+    
+    fileprivate func fetchITunesApps() {
+        
+        Service.shared.fetchApps { (results,err)  in
+            
+            if let err = err {
+                print("Failed to fetch apps: ", err)
+                return
+            }
+            
+            self.appResults = results
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+        }
+        
+    }
+    
+    
+    
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return .init(width: view.frame.width, height: 350)
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return appResults.count
     }
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-//        cell.backgroundColor = .red
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! SearchResultCell
+        
+        cell.appResult = appResults[indexPath.item]
+        
+        
         return cell
     }
     
---
+++ AppStore Clone/Model/SearchResult.swift
@@ -0,0 +1,21 @@
+//
+//  SearchResult.swift
+//  AppStore Clone
+//
+//  Created by Harleen Singh on 29/01/21.
+//
+
+import Foundation
+
+struct SearchResult: Decodable {
+    let resultCount: Int
+    let results: [Result]
+}
+
+struct Result: Decodable {
+    let trackName: String
+    let primaryGenreName: String
+    let averageUserRating: Float?
+    let screenshotUrls: [String]
+    let artworkUrl100: String // app icon
+}
